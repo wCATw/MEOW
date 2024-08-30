d3d969c4fcdf3cca211c1712734fe0b6bacb9794
Lev Kotovskiy <kotovskiy@studio-aw.ru>
2024-08-30 15:49:32 +0300

    mod base

---
+++ addons/main/config.cpp
@@ -0,0 +1,26 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"cba_main","ace_common"};
+        author = "Kotovskiy34";
+        url = "https://github.com/wCATw/MEOW";
+    };
+};
+
+class CfgMods {
+    class PREFIX {
+        dir = "@MEOW";
+        name = "Modify, Enhance, Optimize, Weave";
+        picture = "A3\Ui_f\data\Logos\arma3_expansion_alpha_ca";
+        hidePicture = "true";
+        hideName = "true";
+        actionName = "GitHub";
+        action = "https://github.com/wCATw/MEOW";
+        description = "Meow!";
+    };
+};
---
+++ addons/main/script_mod.hpp
@@ -0,0 +1,26 @@
+// COMPONENT should be defined in the script_component.hpp and included BEFORE this hpp
+
+#define MAINPREFIX z
+#define PREFIX meow
+
+#include "\z\meow\addons\main\script_version.hpp"
+
+#define VERSION     MAJOR.MINOR
+#define VERSION_STR MAJOR.MINOR.PATCHLVL.BUILD
+#define VERSION_AR  MAJOR,MINOR,PATCHLVL,BUILD
+
+// MINIMAL required version for the Mod. Components can specify others..
+#define REQUIRED_VERSION 2.14
+#define REQUIRED_CBA_VERSION {3,16,0}
+#define REQUIRED_ACE_VERSION {3,17,1}
+
+#define COMPONENT_NAME QUOTE(MEOW - COMPONENT_BEAUTIFIED)
+
+// #ifndef COMPONENT_BEAUTIFIED
+//     #define COMPONENT_BEAUTIFIED COMPONENT
+// #endif
+// #ifdef SUBCOMPONENT_BEAUTIFIED
+//     #define COMPONENT_NAME QUOTE(MEOW - COMPONENT_BEAUTIFIED - SUBCOMPONENT_BEAUTIFIED)
+// #else
+//     #define COMPONENT_NAME QUOTE(MEOW - COMPONENT_BEAUTIFIED)
+// #endif
