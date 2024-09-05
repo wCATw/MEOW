bd5b0d1af26638b4bb91ea0082a81a2686d64f51
Lev Kotovskiy <kotovskiy@studio-aw.ru>
2024-09-06 02:27:22 +0300

    CfgVehicles and fix

---
+++ addons/bv_wmo/CfgVehicles.hpp
@@ -0,0 +1,27 @@
+class CfgVehicles {
+    class Items_base_F;
+
+    class BW_roadway_obj : Items_base_F {
+        scope = 2;
+        author = "Bloodwyn";
+        model = QPATHTOF(models\rw.p3d);
+        displayName = CSTRING(Inivisible Roadway LOD for the adaptive roadway);
+        vehicleClass = "Objects";
+        armor = 20000;
+        icon = "iconObject";
+        mapSize = 0.7;
+        accuracy = 0.2;
+    };
+
+    class BW_roadway_obj_debug : Items_base_F {
+        scope = 2;
+        author = "Bloodwyn";
+        model = QPATHTOF(models\rw_debug.p3d);
+        displayName = CSTRING(Inivisible Roadway LOD for the adaptive roadway);
+        vehicleClass = "Objects";
+        armor = 20000;
+        icon = "iconObject";
+        mapSize = 0.7;
+        accuracy = 0.2;
+    };
+};
--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -1,3 +1,15 @@
 #include "\z\meow\addons\main\script_mod.hpp"
 #include "\x\cba\addons\main\script_macros_common.hpp"
 #include "\x\cba\addons\xeh\script_xeh.hpp"
+
+#ifdef DISABLE_COMPILE_CACHE
+    #undef PREP
+    #undef PREP2
+    #define PREP(fncName) DFUNC(fncName) = compile preprocessFileLineNumbers QPATHTOF(functions\DOUBLES(fnc,fncName).sqf)
+    #define PREP2(folder,fncName) DFUNC(fncName) = compile preprocessFileLineNumbers QPATHTOF(functions\#folder\DOUBLES(fnc,fncName).sqf)
+#else
+    #undef PREP
+    #undef PREP2
+    #define PREP(fncName) [QPATHTOF(functions\DOUBLES(fnc,fncName).sqf), QFUNC(fncName)] call CBA_fnc_compileFunction
+    #define PREP2(folder,fncName) [QPATHTOF(functions\#folder\DOUBLES(fnc,fncName).sqf), QFUNC(fncName)] call CBA_fnc_compileFunction
+#endif
